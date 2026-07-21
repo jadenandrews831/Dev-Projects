5b1ed6e0691615bdaf3f4598b5560e7afdb43c5e
Jaden Andrews <jadenandrews@void.void>
2026-07-21 02:47:04 -0400

    Update: game_of_chances.c started

--- C_Projects/AoE/ch2/game_of_chance.c
+++ C_Projects/AoE/ch2/game_of_chance.c
@@ -54,5 +54,121 @@ int main(){
 		printf("[Name: %s]\n", player.name);
 		printf("[You have %u credits] -> ", player.credits);
 		scanf("%d", &choice);
+		if ((choice < 1) || (choice > 7))
+			printf("\n[!!] The number %d is an invalid selection.\n\n", choice);
+		else if (choice < 4){
+			if (choice != last_game){
+				if (choice == 1)
+					player.current_game == pick_a_number;
+				else if (choice == 2)
+					player.current_game == dealer_no_match;
+				else
+					player.current_game == find_the_ace;
+				last_game = choice;
+			}
+			play_the_game();
+		} 
+		else if (choice == 4)
+			show_highscore()
+		else if (choice == 5){
+			printf("\nChange your name\n");
+			printf("Enter your new name: ");
+			input_name();
+			printf("Your name has been changed.\n\n");
+		}
+		else if (choice == 6){
+			printf("\nYour account has been reset with 100 credits.\n\n");
+			player.credits = 100;
+		}
+	}
+	update_player_data();
+	printf("\nThanks for playing! Bye.\n");
+}
+
+int get_player_data(){
+	int fd, uid, read_bytes;
+
+	struct user entry;
+
+	uid = getuid();
+
+	fd = open(DATAFILE, O_RDONLY);
+	if(fd == -1)
+		return -1;
+	read_bytes = read(fd, &entry, sizeof(struct user));
+	while (entry.uid != uid && read_bytes > 0){
+		read_bytes = read(fd, &entry, sizeof(struct user));
+	}
+	close(fd);
+	if(read_bytes < sizeof(struct user)){
+		return -1;
+	}
+	else {
+		player = entry;
+	}
+	return 1;
+}
+
+void register_new_player(){
+	int fd;
+
+	printf("-=-={ New Player Registration }=-=-\n");
+	printf("Enter your name: ");
+	input_name();
+
+	player.uid = getuid();
+	player.highscore = player.credits  = 100;
+
+	fd = open(DATAFILE, O_WRONLY|O_CREAT|O_APPEND, S_IRUSR|S_IWUSR);
+	if(fd == -1)
+		fatal("in register_new_player() while opening file");
+	write(fd, &player, sizeof(struct user));
+	close(fd);
+
+	printf("\nWelcome to the Game of Chance %s.\n", player.name);
+	printf("You have been given %u credits.\n", player.credits);
+}
+
+void update_player_data(){
+	int read_data;
+       	unsigned int uid = getuid();
+
+	struct user temp;
+	fd = open(DATAFILE, O_WRONLY|O_RDONLY);
+	if (fd == -1)
+		fatal("in update_player_data() while opening file");
+
+	read_data = read(fd, &temp, sizeof(struct user));
+	if (read_data == -1)
+		fatal("in update_player_data() while reading from file");
+	while(&temp.uid != uid ){
+		read_data = read(fd, &temp, sizeof(struct user));
+		if (read_data == -1)
+			fatal("in update_player_data() while reading from file");
+	}
+	write(fd, $(player.credits), 4);
+	write(fd, $(player.highscore), 4);
+	write(fd, $(player.name), 100);
+	close(fd);
+}
+
+void show_highscore() {
+	unsigned int top_score = 0;
+	char top_name[100];
+
+	struct user entry;
+
+	int fd;
+
+	printf("\n===================| HIGH SCHORE |===================\n");
+	fd = open(DATAFILE, O_RDONLY);
+	if (fd == -1)
+		fatal("in show_highscore() while opening a file");
+
+	while(read(fd, &entry, sizeof(struct user)) > 0){
+		if (entry.highscore > top_score){
+			top_score = entry.highscore;
+			strcpy(top_name, entry.name)
+		}
 	}
 }
