68a32eb9c64e0386d5edc964be711178370b4196
jadenandrews831 <jadenandrews831@gmail.com>
2026-07-22 17:23:59 -0400

    Generic, tcp/udp server created: Python Projects/server.py

---
+++ Python_Projects/server.py
@@ -0,0 +1,16 @@
+import socket
+import ArgumentParser
+
+def udp_server():
+	pass
+
+def tcp_server():
+	pass
+
+def main():
+	print("-="*8, "Python Adaptable Server", "=-"*8)
+	
+
+
+if __name__ == "__main__":
+	main()
